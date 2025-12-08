3cb74637d1b0fff614c7ed964bb9bef1f3378d1c
Aaron Gustafson <aaron@easy-designs.net>
2025-12-08 08:58:00 -0800

    Harden define

--- define.js
+++ define.js
@@ -1,5 +1,18 @@
 import { FormShowIfElement } from './form-show-if.js';
 
-if (!!customElements) {
-	customElements.define('form-show-if', FormShowIfElement);
+export function defineFormShowIf(tagName = 'form-show-if') {
+	const hasWindow = typeof window !== 'undefined';
+	const registry = hasWindow ? window.customElements : undefined;
+
+	if (!registry || typeof registry.define !== 'function') {
+		return false;
+	}
+
+	if (!registry.get(tagName)) {
+		registry.define(tagName, FormShowIfElement);
+	}
+
+	return true;
 }
+
+defineFormShowIf();
